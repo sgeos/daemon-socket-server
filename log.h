2af078d4178b25616f751528debc6919b61471ec
Brendan A R Sechter <sgeos@hotmail.com>
2015-07-01 14:00:18 +0900

    Commented log.h.

--- log.h
+++ log.h
@@ -32,6 +32,7 @@ extern const char *logLevelString[];
 #define info(format) syslog(LOG_INFO, format)
 #define debug(format) syslog(LOG_DEBUG, format)
 
+// prototypes
 void initLog(const char *pLogName);
 void stopLog();
 
